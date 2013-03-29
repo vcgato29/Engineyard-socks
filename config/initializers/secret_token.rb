acb5b3db05242e4a63fe04cd199c7a88cffde786
= <=>
2013-03-29 17:44:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsocks::Application.config.secret_key_base = 'e67b6da291e79a881549c9296b1c0c6637656cddc3a12d2f46fe501f787fa289e00a72d64e77e11729a1e551137ff9aeae9be234c9936133871c69e533c2009c'
